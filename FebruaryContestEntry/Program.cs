c5e1b877f3d666546efd2e064ffdb8e8c8469cb5
bbrcomputers <alex.caudron@gmail.com>
2020-02-12 16:39:04 +0200

    done!

--- FebruaryContestEntry/Program.cs
+++ FebruaryContestEntry/Program.cs
@@ -6,14 +6,29 @@ namespace FebruaryContestEntry
 	{
 		const char block = '▓';
 
+		const int rows = 15;
+		const int cols = 30;
+		const int first = 0;
 		static void Main(string[] args)
 		{
 			// The solution implementation goes here
-
 			Console.ResetColor();
-			Console.WriteLine();
-			Console.WriteLine();
-			Console.WriteLine("Yay, a colorful shape!");
+			// Process the lines
+			for (var row = first; row <= rows; row++)
+			{
+				// Process all columns for a line
+				for (var col = first; col <= cols; col++)
+				{
+					// Determine the color of the block
+					Console.BackgroundColor = (col == first || col == cols || row == first || row == rows) 
+						? ConsoleColor.Green 
+						: ConsoleColor.Yellow;
+					// Now write the block
+					Console.Write(block);
+				}
+				// Go to the next line on the console
+				Console.WriteLine("");
+			}
 			Console.ReadLine();
 		}
 	}
